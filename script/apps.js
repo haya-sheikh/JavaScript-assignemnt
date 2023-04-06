30cb53e838503b75ba68b8f0b10ecf36909fe45e
haya-sheikh <sheikhhaya344@.com>
2023-04-06 21:43:31 +0500

    FEAT : d1

---
+++ script/apps.js
@@ -0,0 +1,223 @@
+// 6 spril- day 1 
+// alert("Java by haya wants to know your location!"); 
+
+
+// CHAP 2 
+
+// var name = "Haya";
+// name = "Ace";
+// alert(name)
+
+// var nationality = "Australia";
+// alert(nationality)
+
+// var floogle = "Haya";
+// alert(floogle)
+
+// var lessonAuthor = "Haya";
+// var guyWhoKeepsSayingHisOwnName = "Haya";
+// var x = "Haya";
+// alert(x)
+
+// var lastName = "Sheikh";
+// var cityOfOrigin = "Aukland";
+// var aussieGreeting = "g'Day";
+// alert(aussieGreeting)
+
+// var thanx = "Thanks for your input!"
+// alert (thanx);
+
+
+// CHAPTER 3 
+
+
+// var age =+ prompt("23")
+// var fage =+ prompt("7")
+// alert(age+fage)
+// alert(Number(age) + Number(fage))
+// console.log(typeof age)
+
+
+// let x =2
+// let y =5
+// let z =x+y
+// alert(z)
+
+// var originalNumber = 23;
+// var newNumber = originalNumber + 7
+// alert (newNumber)
+
+// var caseQty = 144
+// alert(caseQty)
+
+
+// chapter 5 
+
+// var popularNumber = 2 + 2
+// alert(2 + 2)
+
+// var popularNumber = 12 - 24
+// alert(12 - 24)
+
+// var popularNumber = 3 * 12;
+// alert(3 * 12)
+
+// var num = 10
+// var popularNumber = num + 200
+// alert(popularNumber)
+
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+// alert(popularNumber)
+
+// var whatsLeftOver = 9 % 3;
+// alert(whatsLeftOver)
+
+
+// chapter 6 
+
+// let x = 8;
+// x++;
+// let z = x;
+// alert(z)
+
+// var num = 1;
+// var newNum = num++;
+// alert(newNum)
+
+// var num = 1
+// var newNum = ++num
+// alert(newNum)
+
+// var num = 5;
+// var newNum = num--;
+// alert(newNum)
+
+// var num = 5;
+// var newNum = --num
+// alert(newNum)
+
+
+// chapter 7 
+
+
+// var totalCost = 1 + 3 * 4
+// alert(totalCost)
+
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// alert(resultOfComputation)
+
+// var resultOfComputation = (5 * 6) * 2 + 7
+// alert(resultOfComputation)
+
+
+// chapter 8
+
+// var message = "We wants to know your location";
+// alert(message);
+
+// var message = "Thanks,Haya";
+// var banger = ",";
+// alert(message + banger)
+
+// var message = "Thanks, "
+// var userName = "Haya"
+// var banger = "!"
+// var customMess = message + userName + banger
+// alert(customMess)
+
+// var 2 + 2 = "2 plus 2 equals "
+// alert( 2 + 2)
+
+// alert("2" + "2");
+
+
+// chapter 9 
+
+// var spec = prompt("Your name?", "haya") 
+
+// var question = "Your email?";
+// var defaultAnswer = "abc@gmail.com";
+// var spec = prompt(question, defaultAnswer);
+
+// var numberOfCats = prompt("How much Food?");
+// var tooManyCats = numberOfFood + 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
